082417fc04ba83108bf1f856f40b6ae6aaefb848
Catalin Tanasescu <catalint@gmail.com>
2014-08-12 16:23:33 +0300

    optimised game for ie&firefox

--- inventory/js/ship-laser.js
+++ inventory/js/ship-laser.js
@@ -1,12 +1,10 @@
 Polymer('ship-laser',{
-    hitTest:null,
     startAnimation:function(){
         $(this).animate({left: "100%"},{duration:1000,easing:'linear',complete:function(){
             $(this).remove();
         }});
     },
     attached:function(){
-        this.hitTest = new HitTest( this.$.image );
         this.startAnimation();
     },
     getImage:function(){
